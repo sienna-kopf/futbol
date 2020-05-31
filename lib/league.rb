604060403f5554df9dbf58d3baf0cf135875e38f
Amy Moller <amy.moller@yahoo.com>
2020-05-31 17:04:28 -0600

    Add addtl init method to league class

--- lib/league.rb
+++ lib/league.rb
@@ -1,19 +1,21 @@
 require 'csv'
 class League
 
-  attr_reader :away_team_id,
+  attr_reader :season,
+              :away_team_id,
               :home_team_id,
               :away_goals,
               :home_goals,
 
   def initialize(league_data)
+    @season = league_data[:season].to_s
     @away_team_id = league_data[:away_team_id].to_i
     @home_team_id = league_data[:home_team_id].to_i
     @away_goals = league_data[:away_goals]
     @home_goals = league_data[:home_goals]
   end
 
-end 
+end
 
 
 
