8975c8c33e1c6b044284e879dc5404570009592d
Amy Moller <amy.moller@yahoo.com>
2020-05-31 19:14:04 -0600

    Scrapped initialize method for league class

--- lib/league.rb
+++ lib/league.rb
@@ -1,17 +1,12 @@
 require 'csv'
 class League
-  attr_reader :season,
-              :away_team_id,
-              :home_team_id,
-              :away_goals,
-              :home_goals,
 
-  def initialize(league_data)
-    @season = league_data[:season].to_s
-    @away_team_id = league_data[:away_team_id].to_i
-    @home_team_id = league_data[:home_team_id].to_i
-    @away_goals = league_data[:away_goals]
-    @home_goals = league_data[:home_goals]
+
+  def initialize(games, game_teams, teams)
+    @games = games
+    @game_teams = game_teams
+    @teams = teams
+
   end
 
 end
