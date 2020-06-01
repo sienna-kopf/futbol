d81ff3c0523f829432f344fef940fbdc6fb08fa2
Amy Moller <amy.moller@yahoo.com>
2020-05-31 18:03:29 -0600

    Add all league test titles

--- test/league_test.rb
+++ test/league_test.rb
@@ -16,20 +16,36 @@ class LeagueTest < Minitest::Test
   end
 
   def test_it_exists
-    skip 
+    skip
   end
 
+  def test_it_has_count_of_teams
+    skip
+  end
 
+  def test_it_has_best_offense
+    skip
+  end
 
+  def test_it_has_worst_offense
+    skip
+  end
 
+  def test_it_has_highest_scoring_visitor
+    skip
+  end
 
+  def test_it_has_highest_scoring_home_team
+    skip
+  end
 
+  def test_it_has_lowest_scoring_visitor
+    skip
+  end
 
-
-
-
-
-
+  def test_it_has_lowest_scoring_home_team
+    skip
+  end
 
 
 
