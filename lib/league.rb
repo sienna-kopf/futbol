7e3a28d684eaa2ce944e88a7e7132fb688f9083f
Amy Moller <amy.moller@yahoo.com>
2020-06-01 08:34:58 -0600

    Reset initialize data for league class to previous

--- lib/league.rb
+++ lib/league.rb
@@ -2,11 +2,12 @@ require 'csv'
 class League
 
 
-  def initialize(games, game_teams, teams)
-    @games = games
-    @game_teams = game_teams
-    @teams = teams
-
+  def initialize(league_data)
+    @season = league_data[:season].to_s
+    @away_team_id = league_data[:away_team_id].to_i
+    @home_team_id = league_data[:home_team_id].to_i
+    @away_goals = league_data[:away_goals]
+    @home_goals = league_data[:home_goals]
   end
 
 end
