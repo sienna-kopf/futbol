dba3f1dad8c2fc39a1580625991571e9494a743d
Amy Moller <amy.moller@yahoo.com>
2020-05-31 18:14:42 -0600

    Add first rough league tests

--- test/league_test.rb
+++ test/league_test.rb
@@ -4,47 +4,50 @@ require './lib/league'
 
 class LeagueTest < Minitest::Test
   def setup
+    @league = League.new
 
 
 
-
-
-
-
-
-
-  end
+end
 
   def test_it_exists
     skip
+    assert_instance_of League, @league
   end
 
   def test_it_has_count_of_teams
     skip
+    assert_equal 10, @teams.count 
   end
 
   def test_it_has_best_offense
     skip
+
   end
 
   def test_it_has_worst_offense
     skip
+
   end
 
   def test_it_has_highest_scoring_visitor
     skip
+
   end
 
   def test_it_has_highest_scoring_home_team
     skip
+
   end
 
   def test_it_has_lowest_scoring_visitor
     skip
+
   end
 
   def test_it_has_lowest_scoring_home_team
     skip
+
   end
 
 
